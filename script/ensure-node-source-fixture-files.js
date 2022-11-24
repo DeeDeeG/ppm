170f4c94458bb8fe5895555349401f0e4838f3b8
DeeDeeG <DeeDeeG@users.noreply.github.com>
2022-11-24 03:51:21 -0500

    lint: Codacy suggestion: Single quote -> Double quote
    Co-authored-by: Spiker985 <7829451+Spiker985@users.noreply.github.com>

--- script/ensure-node-source-fixture-files.js
+++ script/ensure-node-source-fixture-files.js
@@ -1,33 +1,33 @@
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
 
 // This "nodeVersion" specifies the version of Node used by certain specs.
 // Update nodeVersion here to run those specs against newer Node source files.
 // (May be needed to work around build issues with newer Node bundled with ppm.)
 // This string is also hard-coded in those spec files, so update it there, too.
-const nodeVersion = 'v18.12.1'
+const nodeVersion = "v18.12.1"
 
 const filesToFetch = [
   {url: `https://nodejs.org/dist/${nodeVersion}/node-${nodeVersion}.tar.gz`,
   filename: `node-${nodeVersion}.tar.gz`,
-  sha256sum: 'ba8174dda00d5b90943f37c6a180a1d37c861d91e04a4cb38dc1c0c74981c186'},
+  sha256sum: "ba8174dda00d5b90943f37c6a180a1d37c861d91e04a4cb38dc1c0c74981c186"},
   {url: `https://nodejs.org/dist/${nodeVersion}/node-${nodeVersion}-headers.tar.gz`,
   filename: `node-${nodeVersion}-headers.tar.gz`,
-  sha256sum: '9d55ee072ba6d5a141db092cef1a0f715f7d3fc938285a6d927a1d0a0c7442f7'},
+  sha256sum: "9d55ee072ba6d5a141db092cef1a0f715f7d3fc938285a6d927a1d0a0c7442f7"},
   {url: `https://nodejs.org/dist/${nodeVersion}/win-x64/node.lib`,
-  filename: 'node_x64.lib',
-  sha256sum: '1bd376a23d181d85096d1a9c46e6be7fcd20d30f9b8f77a2a847d3dbff8e25c7'},
+  filename: "node_x64.lib",
+  sha256sum: "1bd376a23d181d85096d1a9c46e6be7fcd20d30f9b8f77a2a847d3dbff8e25c7"},
   {url: `https://nodejs.org/dist/${nodeVersion}/win-x86/node.lib`,
-  filename: 'node.lib',
-  sha256sum: 'b1c6dc670911d85ef1704fa56f4cc4c7e1071f4869778398e6d88b3b0b565978'},
+  filename: "node.lib",
+  sha256sum: "b1c6dc670911d85ef1704fa56f4cc4c7e1071f4869778398e6d88b3b0b565978"},
   {url: `https://nodejs.org/dist/${nodeVersion}/SHASUMS256.txt`,
-  filename: 'SHASUMS256.txt',
-  sha256sum: '64aad1211a6003dd6529ebf9f19167769d0356ce5affc4245bb26c35aa66a9ed'},
+  filename: "SHASUMS256.txt",
+  sha256sum: "64aad1211a6003dd6529ebf9f19167769d0356ce5affc4245bb26c35aa66a9ed"},
 ] // If you ever need to update to a newer Node version, just calculate the sha256sums
   // for the new files and update them here to match the new files.
 
-const sourceFixtureDir = path.resolve(__dirname, '..', 'spec', 'fixtures', 'node-source');
+const sourceFixtureDir = path.resolve(__dirname, "..", "spec", "fixtures", "node-source");
 fs.mkdirSync(sourceFixtureDir, {recursive: true});
 
 for (const details of filesToFetch) {
@@ -39,7 +39,7 @@ async function ensureFile (details) {
   // make sure its sha256sum matches the expected/correct value.
   // If it doesn't exist or has the wrong hash, try to re-download.
 
-  logVerbose('details is:');
+  logVerbose("details is:");
   logVerbose(details);
 
   const destinationPath = path.resolve(sourceFixtureDir, details.filename);
@@ -48,12 +48,12 @@ async function ensureFile (details) {
   const existingFileIsCorrect = await verifyExistingFile(destinationPath, details.sha256sum);
 
   if (!existingFileIsCorrect) {
-    logVerbose('Hash did not match, re-downloading...')
+    logVerbose("Hash did not match, re-downloading...")
     // Get the file
     downloadFileToDestination(details.url, destinationPath, details.sha256sum)
       .then(async function () {
         console.log(`Successfully downloaded file from ${details.url}.`)
-        logVerbose('checking if hash matches in for...of loop.');
+        logVerbose("checking if hash matches in for...of loop.");
         // Check its hash
         const hashDidMatch = await verifyHash(destinationPath, details.sha256sum);
         if (!hashDidMatch) {
@@ -95,11 +95,11 @@ async function verifyHash (path, expectedHash) {
 
   // This module apparently needs to be required each time this function runs,
   // or hashing multiple files in parallel can get messed up? Not sure why.
-  const { createHash } = require('node:crypto');
+  const { createHash } = require("node:crypto");
 
-  const hash = createHash('sha256');
+  const hash = createHash("sha256");
   hash.update(fs.readFileSync(path))
-  const actualHash = hash.digest('hex');
+  const actualHash = hash.digest("hex");
   hash.end()
   // The hash of the actual file on disk.
 
@@ -108,10 +108,10 @@ async function verifyHash (path, expectedHash) {
   // The expected hash is from the array of objects at the top of the file.
 
   if (actualHash === expectedHash) {
-    logVerbose('Hash verified successfully');
+    logVerbose("Hash verified successfully");
     return true;
   } else {
-    logVerbose('Hash did not match');
+    logVerbose("Hash did not match");
     return false;
   }
 }
@@ -129,8 +129,8 @@ function downloadFileToDestination(url, filePath, expectedHash) {
       // but in practice this should always be exactly "200"? Adjust if needed.
       if (res.statusCode === 200) {
         res.pipe(fs.createWriteStream(filePath))
-          .on('error', reject)
-          .once('close', resolve);
+          .on("error", reject)
+          .once("close", resolve);
       } else {
         // Consume response data to free up memory
         res.resume();
